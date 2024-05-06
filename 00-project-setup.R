b4b67615e84972fa0aef482e90eb765b387695ea
Nick Mader <nsmader@gmail.com>
2024-05-06 00:40:52 -0400

    Add load of packages for display

--- 00-project-setup.R
+++ 00-project-setup.R
@@ -1,4 +1,6 @@
-packages_list <- c("tidyverse", "openxlsx", "flextable")
+packages_list <- 
+  c("tidyverse", "openxlsx", "flextable", "broom", "glue", "scales", "gt",
+    "gtsummary", "tidyr")
 
 # Load packages, and install if not found among already-installed packages
 for (p in packages_list) {
